0def184a0d050c57573a475b2dae53f99710e26f
DangVinh22 <vd4383809@gmail.com>
2026-03-11 14:10:50 +0700

    Initial commit

---
+++ Models/Link.cs
@@ -0,0 +1,9 @@
+﻿namespace LinkMaker.Models
+{
+    public class Link
+    {
+        public int Id { get; set; }
+        public string curUrl { get; set; }
+        public string newUrl { get; set; }
+    }
+}
